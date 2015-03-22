58772cedaa76583b285391668edb4cf968f3a00a
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-22 18:42:50 +0800

    authorize access token

--- routes/authorize.js
+++ routes/authorize.js
@@ -5,7 +5,6 @@ var format = require('util').format;
 var key = '2627706825'
 var secret = '773eb51c7411b179002758188b92da92'
 var reuri = 'zjq.101a.net/authorize/getcode'
-var http = require("http")
 
 function getAccessToken(code, callback) {
 
@@ -29,7 +28,7 @@ function getAccessToken(code, callback) {
             "Content-Length": data.length
         }
     };
-
+    var http = require("http")
     var req = http.request(opt, function (serverFeedback) {
         if (serverFeedback.statusCode == 200) {
             var body = "";
