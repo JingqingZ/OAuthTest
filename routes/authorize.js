c7a1acd719e1b5cc264220ccf7335dd96afec91c
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-22 15:43:27 +0800

    add authorize

--- routes/authorize.js
+++ routes/authorize.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('https://api.weibo.com/oauth2/authorize?client_id=2335893352&response_type=code&redirect_uri=zjq.101a.net/authorize/getcode');
+  res.redirect('https://api.weibo.com/oauth2/authorize?client_id=2627706825&response_type=code&redirect_uri=zjq.101a.net/authorize/getcode');
+  //secret 773eb51c7411b179002758188b92da92
 });
 
 router.get('/getcode', function(req, res){
-	
+	var geturl = require('url').parse(req.url, true)
+	var code = geturl.query.code.toString()
+	console.log(code);
+	res.end("nothing")
 })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,8 @@
 var express = require('express');
 var router = express.Router();
-/*var mongoose = require("mongoose");
-var db = mongoose.createConnection('mongodb://127.0.0.1:27017/userdb');
-
-// 链接错误
-db.on('error', function(error) {
-    console.log(error);
-});
-
-// Schema 结构
-var mongooseSchema = new mongoose.Schema({
-    username : {type : String, default : '匿名用户'},
-    title    : {type : String},
-    content  : {type : String},
-    time     : {type : Date, default: Date.now},
-    age      : {type : Number}
-});
-
-// 添加 mongoose 实例方法
-mongooseSchema.methods.findbyusername = function(username, callback) {
-    return this.model('mongoose').find({username: username}, callback);
-}
-
-// 添加 mongoose 静态方法，静态方法在Model层就能使用
-mongooseSchema.statics.findbytitle = function(title, callback) {
-    return this.model('mongoose').find({title: title}, callback);
-}
-
-// model
-var mongooseModel = db.model('mongoose', mongooseSchema);
-
-// 增加记录 基于 entity 操作
-var doc = {username : 'jingqingz', title : 'chairman', content : 'zjq is nb', age: 20};
-var mongooseEntity = new mongooseModel(doc);
-mongooseEntity.save(function(error) {
-    if(error) {
-        console.log(error);
-    } else {
-        console.log('saved OK!');
-    }
-    // 关闭数据库链接
-    db.close();
-});*/
-
- var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
+var MongoClient = require('mongodb').MongoClient
+var format = require('util').format;
  
-  
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
