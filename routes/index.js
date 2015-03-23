67ccba7b5477cfee553a5f70e1d19e6196ca8f0c
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-23 13:37:34 +0800

    authorize

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,23 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient
 var format = require('util').format;
-var https = require('https')
+var https = require('https');
+var bl = require('bl');
 
 function getWeibo(access_token, uid, since_id, max_id, count, callback){
 	https.get("https://api.weibo.com/2/statuses/user_timeline.json?access_token="+access_token+"&uid="+uid+"&since_id="+since_id+"&max_id="+max_id+"&count="+count, 
 			function(res) {
 		console.log("Got response: " + res.statusCode);
-		callback(null, res);
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
-		callback(e, null)
+		res.pipe(bl(function(err, data){
+			if (err) {
+				callback(err, null);
+			}else{
+				callback(null, res);
+			}
+		}))
+	}).on('error', function(err) {
+		console.log("Got error: " + err.message);
+		callback(err, null)
 	});
 }
 
