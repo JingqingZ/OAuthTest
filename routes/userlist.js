f59d63977c63227b99851e40d726ce2a083864e0
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-24 13:31:45 +0800

    users

--- routes/userlist.js
+++ routes/userlist.js
@@ -6,23 +6,60 @@ var format = require('util').format;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
-        if(err) throw err;
-     
-        var collection = db.collection('test_insert');
-        /*collection.insert({a:2}, function(err, docs) {
-            collection.count(function(err, count) {
-                console.log(format("count = %s", count));
+	MongoClient.connect('mongodb://127.0.0.1:27017/weibodb', function(err, db) {
+        if (err) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: {}
             });
-        */
-            // Locate all the entries using find 
+        } else {
+            var allusers = []
+            var collection = db.collection('users');
             collection.find().toArray(function(err, results) {
-                console.dir(results);
-                // Let's close the db 
-                db.close();
-                res.render('userlist', {"userlist": results})
-            });
-        //});
+                //console.dir(results);
+                results.forEach(function(userinfo){
+                    if(typeof(userinfo.access_token) == 'undefined'){
+                        collection.remove(userinfo);
+                    } else {
+                        allusers[allusers.length] = userinfo;
+                    }
+                });
+                //res.end(JSON.stringify(allusers));
+                async.map(allusers, function(item, done){
+                    weiboapi.getUser(item.access_token, item.uid, function(err, user){
+                        if (err) {
+                            done(err);
+                        } else {
+                            user = JSON.parse(user);
+                            //console.log(JSON.stringify(user));
+                            done(null, user);
+                        }
+                    })
+                },
+                function(err, user){
+                    if (err) {
+                        res.status(err.status || 500);
+                        res.render('error', {
+                            message: err.message,
+                            error: {}
+                        });
+                    } else {
+                        var users = [];
+                        //console.log(JSON.stringify(info[0][1]))
+                        for (var i = 0; i < user.length; i++) {
+                            if(user[i] == null)
+                                continue;
+                            users = users.concat(user[i]);
+                        };
+                        //res.end(JSON.stringify(stat));
+                        res.render('userlist', {"userlist": users});
+                    }
+                    // Let's close the db 
+                    db.close();
+                })
+             });
+        }
     })
 });
 
