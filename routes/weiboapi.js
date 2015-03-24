383ae16353cee669a62be24a984f4cc914349e07
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-24 13:25:23 +0800

    users

--- routes/weiboapi.js
+++ routes/weiboapi.js
@@ -1,7 +1,7 @@
 var https = require('https');
 var bl = require('bl');
 
-function getWeibo(access_token, uid, since_id, max_id, count, callback){
+exports.getWeibo = function (access_token, uid, since_id, max_id, count, callback){
 	https.get("https://api.weibo.com/2/statuses/user_timeline.json?access_token="+access_token+"&uid="+uid+"&since_id="+since_id+"&max_id="+max_id+"&count="+count, 
 			function(res) {
 		//console.log("Got response: " + res.statusCode);
@@ -18,7 +18,7 @@ function getWeibo(access_token, uid, since_id, max_id, count, callback){
 	});
 }
 
-function getUser(access_token, uid, callback){
+exports.getUser = function(access_token, uid, callback){
 	https.get("https://api.weibo.com/2/users/show.json?access_token="+access_token+"&uid="+uid, 
 			function(res) {
 		console.log("Got user response: " + res.statusCode);
