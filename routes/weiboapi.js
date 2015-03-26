44b70cf071499e90eaec6b95e226b86f8ac3e4ee
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-26 11:05:38 +0800

    posts

--- routes/weiboapi.js
+++ routes/weiboapi.js
@@ -38,7 +38,7 @@ exports.getKeyword = function(statuses, callback){
 	for (var i = 0; i < statuses.length; i++) {
 		text += statuses[i].text;
 	};
-	text = text.replace(/\//, "");
+	text = text.replace(/\//g, "");
 	console.log(text);
 	http.get("http://api.yutao.us/api/keyword/" + text, function(res) {
 		res.pipe(bl(function(err, keyword){
