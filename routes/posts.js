d0f5d181899c11867ad6bc30a7fcced5f9e4c4f2
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-26 16:35:23 +0800

    posts

--- routes/posts.js
+++ routes/posts.js
@@ -11,46 +11,44 @@ router.get('/show', function(req, res) {
     res.redirect("/")
 })
 
-router.get('^/[0-9]+/show$', function(req, res) {
-	var geturl = require('url').parse(req.url, true).pathname.split('/');
-	geturl = geturl[1];
-    MongoClient.connect('mongodb://127.0.0.1:27017/weibodb', function(err, db) {
+function getPosts(id, callback){
+	MongoClient.connect('mongodb://127.0.0.1:27017/weibodb', function(err, db) {
 		if (err) {
-			res.render("posts", {"err": "database"});
+			callback("database", null, null, null);
 		} else {
 			var collection = db.collection("users");
-			collection.find({"uid":geturl}).toArray(function(err, results) {
+			collection.find({"uid":id}).toArray(function(err, results) {
 				if(err) {
 					console.log("Database Error! " + err.message);
-					res.render("posts", {"err": "database"});
+					callback("database", null, null, null);
 				} else if(results == null || results.length <= 0){
-					res.render("posts", {"err": "nosuchuser"});
+					rcallback("nosuchuser", null, null, null);
 				} else {
 					weiboapi.getWeibo(results[0].access_token, results[0].uid, 0, 0, 100, function(err, data){
 						if(err) {
-							res.render("posts", {"err": "weibonotreach"});
+							callback("weibonotreach", null, null, null);
 						} else {
 							try {
 								data = JSON.parse(data)
 							} catch(err) {
-								res.render("posts", {"err": "weibonotreach"});
+								callback("weibonotreach", null, null, null);
 								return;
 							}
 							if(typeof(data.statuses) == 'undefined'){
-				    			res.render("posts", {"err": "weibonotreach"});
+				    			callback("weibonotreach", null, null, null);
 				    		} else {
 				    			weiboapi.getUser(results[0].access_token, results[0].uid, function(err, user){
 				    				if (err) {
-										res.render("posts", {"err": "weibonotreach"});
+										callback("weibonotreach", null, null, null);
 									} else {
 										try {
 											user = JSON.parse(user)
 										} catch(err) {
-											res.render("posts", {"err": "weibonotreach"});
+											callback("weibonotreach", null, null, null);
 											return;
 										}
 										if(typeof(user.id) == 'undefined'){
-											res.render("posts", {"err": "weibonotreach"});
+											callback("weibonotreach", null, null, null);
 										} else {
 											var keyword = []
 											weiboapi.getKeyword(data.statuses, function(err, kw){
@@ -67,7 +65,7 @@ router.get('^/[0-9]+/show$', function(req, res) {
 														keyword = kw.split(',');
 													}
 												}
-												res.render("posts", {"err": "success", "data": data.statuses, "user": user, "keyword": keyword});
+												callback("success", data.statuses, user, keyword});
 											})
 										}
 									}
@@ -80,6 +78,18 @@ router.get('^/[0-9]+/show$', function(req, res) {
 			});		
 		}
 	});
+}
+
+router.get('^/[0-9]+/show$', function(req, res) {
+	var geturl = require('url').parse(req.url, true).pathname.split('/');
+	geturl = geturl[1];
+    getPosts(geturl, function(err, statuses, user, keyword){
+    	if (err != "success") {
+    		res.render("posts", {"err": err});
+    	} else {
+    		res.render("posts", {"err": "success", "data": statuses, "user": user, "keyword": keyword});
+    	}
+    })
 })
 
 module.exports = router;
